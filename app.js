30483bb95ea8277f86f3df868172c5f1ac668f7d
春威 朱 <position01p@hotmail.com>
2021-01-23 09:08:41 +0800

    vue practive

---
+++ app.js
@@ -0,0 +1,12 @@
+const app=new Vue({
+    el:"#app",
+    data:{
+        admainName:'綺忻',
+        bio:'這是簡歷內容',
+        tweets:['我想跟大家介紹...',
+                '今天的話題是...',
+                '時間都在...',
+    ],
+        newTweet:'',    
+    }
+})
